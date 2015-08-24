5ab3f74cae969e864ba7220c849713d8aaf61748
Joe Barnes <barnesjd@gmail.com>
2015-08-24 09:01:59 -0500

    Forgot the output the public subnets

--- vpc-2-zones/outputs.tf
+++ vpc-2-zones/outputs.tf
@@ -13,6 +13,18 @@ output "zone_B_private_id" {
 output "zone_B_private_cidr_block" {
   value = "${aws_subnet.private-B.cidr_block}"
 }
+output "zone_A_public_id" {
+  value = "${aws_subnet.public-A.id}"
+}
+output "zone_A_public_cidr_block" {
+  value = "${aws_subnet.public-A.cidr_block}"
+}
+output "zone_B_public_id" {
+  value = "${aws_subnet.public-B.id}"
+}
+output "zone_B_public_cidr_block" {
+  value = "${aws_subnet.public-B.cidr_block}"
+}
 output "packer_sg_id" {
   value = "${aws_security_group.packer.id}"
 }
