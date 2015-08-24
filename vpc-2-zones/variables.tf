11140bbf58c734201f64f618bb727e4a6c3e914f
Joe Barnes <barnesjd@gmail.com>
2015-08-24 08:57:59 -0500

    Committed the wrong variables file last time

--- vpc-2-zones/variables.tf
+++ vpc-2-zones/variables.tf
@@ -1,23 +1,10 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "vpc_name" {}
+variable "ec2_key_name" {}
 
-# All of the following variables must change if you want to use a different AWS region,
-# including the AMIs.
-variable "region" {
-  default = "us-west-1"
-}
-variable "zone_A" {
-  default = "us-west-1b"
-}
-variable "zone_B" {
-  default = "us-west-1c"
-}
-variable "nat_ami" {
-	default = "ami-ada746e9"
-}
-variable "bastion_ami" {
-	default = "ami-896d93cd"
-}
-variable "key_name" {
-  default = "sandbox"
-}
+# Region-dependent variables
+variable "region" {}
+variable "zone_A" {}
+variable "zone_B" {}
+variable "nat_ami" {}
+variable "bastion_ami" {}
+
