fede1a256ee53b2da3ec3ce60bbdde6907778b0e
Joe Barnes <barnesjd@gmail.com>
2015-08-30 13:00:13 -0500

    Taking a stab at making a region module

---
+++ us-west-1/region.tf
@@ -0,0 +1,18 @@
+output "region" {
+  value = "us-west-1"
+}
+output "zone_A" {
+  value = "us-west-1b"
+}
+output "zone_B" {
+  value = "us-west-1c"
+}
+output "nat_ami" {
+	value = "ami-ada746e9"
+}
+output "bastion_ami" {
+	value = "ami-896d93cd"
+}
+output "ubuntu_precise_12_04_amd64" {
+	value = "ami-4bcf360f"
+}
