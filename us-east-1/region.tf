87cf30f3b0f1977874e6ab3a8ab7e41c2bd69597
Joe Barnes <barnesjd@gmail.com>
2016-03-20 17:15:01 +0000

    Defining us-east-1

---
+++ us-east-1/region.tf
@@ -0,0 +1,24 @@
+output "region" {
+  value = "us-east-1"
+}
+output "zone_A" {
+  value = "us-east-1b"
+}
+output "zone_B" {
+  value = "us-east-1d"
+}
+output "zone_C" {
+  value = "us-east-1e"
+}
+output "nat_ami" {
+	value = "ami-3ff7f255"
+}
+output "bastion_ami" {
+	value = "ami-c80b0aa2"
+}
+output "ubuntu_precise_12_04_amd64" {
+	value = "ami-75dadb1f"
+}
+output "amazon_linux_hvm_ebs" {
+	value = "ami-8fcee4e5"
+}
